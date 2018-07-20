4bafb976b8e2114123891122bcbb213227bca07f
CADM2707 <alex.diaz.2707@gmail.com>
2018-07-20 12:03:36 -0500

    Pantilla V02

--- index.php
+++ index.php
@@ -23,7 +23,11 @@
                 <section class="content" >
                 <!-- Small boxes (Stat box) -->
                 <div class="row">
-                    <div class="col-lg-12 col-xs-12 text-center">                        
+                    <div class="col-lg-12 col-xs-12 text-center">   
+                        <div id="cont">
+                            Hola Soy un toggle
+                        </div>
+                        <input type="button" value="BUTTON" id="btn">
                         <!--area de trabajo-->
 <!--                    ***
                         ***
@@ -41,6 +45,47 @@
             </div>
             
             <?php include_once 'footer.html'; ?>
+            <script>
+//                Funcion con ajax sinn formulario
+//                function buttonGraf(sec) {
+//                    
+//                    var url = "<?php echo BASE_URL; ?>includes/facturacionEmitida/responseGrafFacEm.php";
+//
+//                    $.ajax({
+//                        type: "POST",
+//                        url: url,
+//                        data: {
+//                            UnoCal1: $('#UnoCal1').val(),                            
+//                            Sector: sec
+//                        },
+//                        success: function (data)
+//                        {
+//                            $("#tb2").html(data); // Mostrar la respuestas del script PHP.
+//                            document.getElementById("tb2").style.display="block";                               
+//                        }
+//                    });
+//                    document.getElementById("Sec").value = sec;                              
+//                    return false;
+//                }
+//*****************************************************************************************************************************
+
+//        AJAX CON FORMULARIO
+//    $('#formTb1').submit(function () {
+//        var url = "<?php echo BASE_URL; ?>includes/facturacionEmitida/responseFactEm.php";
+//        $.ajax({
+//            type: "POST",
+//            url: url,
+//            data: $("#formTb1").serialize(), // Adjuntar los campos del formulario enviado.
+//            success: function (data)
+//            {
+//                $("#tb1").html(data); // Mostrar la respuestas del script PHP.    
+//                document.getElementById('tb2').style.display = "none";                                                             
+//            }
+//        });
+//
+//        return false;
+//    });
+            </script>
 
 
 
