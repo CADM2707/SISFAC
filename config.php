5736d430b80e927e19cdcf3852c5a7c2501c3b6b
CADM2707 <alex.diaz.2707@gmail.com>
2018-07-23 19:10:42 -0500

    menu lateral

--- config.php
+++ config.php
@@ -1,18 +1,18 @@
 <?php
 session_start();
 
-if (isset($_SESSION['CVE_PERFIL'])) {
-    $perfil=$_SESSION['CVE_PERFIL'];
+if (isset($_SESSION['PLACA'])) {    
     $nombre=$_SESSION['NOMBRE'];
-    $Apep=$_SESSION['APELLIDOP'];
-    $ApeM=$_SESSION['APELLIDOM'];
+    $sec=$_SESSION['SECTOR'];
+    $dest=$_SESSION['DEST'];
+    include('conexiones/sqlsrv.php');
+    $conn = connection_object();
 } else {
 //    echo("<script>window.location.replace(".BASE_URL."'Login.php');</script>");
     define('BASE_URL2', 'http://' . $_SERVER['SERVER_NAME'] . '/SISFAC/');
     header('Location:'.BASE_URL2.'Login.php');
 }
     	    
-	include('conexiones/sqlsrv.php');
-	$conn = connection_object();
+	
         
  
--- menuLat.php
+++ menuLat.php
@@ -13,17 +13,31 @@
                         </div>
                     </div>
                     <ul class="sidebar-menu" data-widget="tree">
-                        <li class="header text-center" style="font-weight: 600;">MENU PRINCIPAL</li>                                       
-<!--                        <li>
-                            <a href="#">
-                                <i class="fa fa-th"></i> <span>Widgets</span>
-                                <span class="pull-right-container">
-                                    <small class="label pull-right bg-green">new</small>
-                                </span>
-                            </a>
-                        </li>                       -->
-
-                        <li class="treeview">
+                         <li class="header text-center" style="font-weight: 600;"> MENÚ</li>
+                        
+                        <?php
+                            $query="select T2.ID_PROGRAMA,CVE_PERFIL,CVE_GRUPO,MODULO,ARCHIVO,ICONO,CARPETA 
+                                    FROM BITACORA.DBO.Operador_Padron  T1 
+                                    INNER JOIN BITACORA.DBO.Programa_Perfil T2 ON T1.ID_OPERADOR=T2.ID_OPERADOR 
+                                    INNER JOIN BITACORA.DBO.Operador_Grupo T3 ON T1.ID_OPERADOR=T3.ID_OPERADOR AND T2.ID_PROGRAMA=T3.ID_PROGRAMA
+                                    INNER JOIN   [Facturacion].[dbo].[PRGMODULO] T4 ON T2.ID_PROGRAMA=T4.ID_PROGRAMA AND T2.CVE_PERFIL=T4.ID_PERFIL
+                                     WHERE T1.ID_OPERADOR=1080461 order by CARPETA ";
+                            
+                            $execue=sqlsrv_query($conn,$query);
+                            $carpeta="";
+                            while($row=sqlsrv_fetch_array($execue)){
+                                if($carpeta == $row['CARPETA']){ ?>
+                                 
+                         <li><a href="#"><i class="fa <?php echo $row['ICONO']; ?> text-aqua"></i> <span><?php echo utf8_encode($row['MODULO']); ?></span></a></li>
+                                <?php                                 
+                                }else{ ?>
+                                    <li class="header text-center" style="font-weight: 600;"><?php echo $row['CARPETA'];?></li>
+                                    <li><a href="#"><i class="fa <?php echo $row['ICONO']; ?> text-aqua"></i> <span><?php echo utf8_encode($row['MODULO']); ?></span></a></li>
+                                <?php }
+                                $carpeta = $row['CARPETA'];
+                            }
+                        ?>
+<!--                        <li class="treeview">
                             <a href="#">
                                 <i class="fa fa-folder"></i> <span>Examples</span>
                                 <span class="pull-right-container">
@@ -41,7 +55,7 @@
                                 <li><a href="pages/examples/blank.html"><i class="fa fa-circle-o"></i> Blank Page</a></li>
                                 <li><a href="pages/examples/pace.html"><i class="fa fa-circle-o"></i> Pace Page</a></li>
                             </ul>
-                        </li>                       
+                        </li>                       -->
 <!--                        <li class="treeview">
                             <a href="#">
                                 <i class="fa fa-folder"></i> <span>Examples</span>
