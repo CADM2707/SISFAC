134a05b8fd46b995588ead2d29807acfdff96f44
CADM2707 <alex.diaz.2707@gmail.com>
2018-08-02 12:25:36 -0500

    fac_txt Alexis

--- FACTURACION/fac_txt.php
+++ FACTURACION/fac_txt.php
@@ -1,9 +1,8 @@
 <?php
     include_once '../config.php';
     include_once '../head.html';
-    include_once '../menuLat.php';
-	
-?>     
+    include_once '../menuLat.php';	
+?>
 
 <script language="javascript" type="text/javascript">
   function es_vacio(){
@@ -51,19 +50,19 @@
 	  }
 	}
   </script>
-  
-   <?php 
+
+   <?php
   //CONSULTAS	---		CONSULTAS	---		CONSULTAS	---		CONSULTAS	---
-	$sql_ayo="select DISTINCT(ayo)  from seCTOR.DBO.C_PERIODO_QNAS";       
-	$res_ayo = sqlsrv_query($conn,$sql_ayo); 
-			  
-	$sql_qna="select DISTINCT(Qna)  from seCTOR.DBO.C_PERIODO_QNAS";       
-	$res_qna = sqlsrv_query($conn,$sql_qna); 
-	
-	$sql_sector="select SECTOR from sector.dbo.C_Sector where SECTOR>50 GROUP BY SECTOR ORDER BY SECTOR";       
-	$res_sector = sqlsrv_query($conn,$sql_sector); 			
+	$sql_ayo="select DISTINCT(ayo)  from seCTOR.DBO.C_PERIODO_QNAS";
+	$res_ayo = sqlsrv_query($conn,$sql_ayo);
+
+	$sql_qna="select DISTINCT(Qna)  from seCTOR.DBO.C_PERIODO_QNAS";
+	$res_qna = sqlsrv_query($conn,$sql_qna);
+
+	$sql_sector="select SECTOR from sector.dbo.C_Sector where SECTOR>50 GROUP BY SECTOR ORDER BY SECTOR";
+	$res_sector = sqlsrv_query($conn,$sql_sector);
   ?>
-                   
+
             <!-- Content Wrapper. Contains page content -->
             <div class="content-wrapper" style=" background-color: white;">
                 <!--Titulos de encabezado de la pagina-->
@@ -71,25 +70,25 @@
                     <h1>
                         TIMBRADO
                         <small></small>
-                    </h1>                    
+                    </h1>
                     <br>
                 </section>
                 <!-- FIN DE Titulos de encabezado de la pagina-->
-                
+
                 <section class="content" >
                 <!-- Small boxes (Stat box) -->
                 <div class="row">
-                    <div class="col-lg-12 col-xs-12 text-center">   
+                    <div class="col-lg-12 col-xs-12 text-center">
 						<div  class="col-md-3 col-sm-3 col-xs-3">	<br>
-							<center><label>SECTOR:</label></center>				
+							<center><label>SECTOR:</label></center>
 							<select name="sector" class="form-control" id="sector">
 								<option value="" selected="selected">SELECC...</option>
-								<?php	while($row_sector = sqlsrv_fetch_array($res_sector)){ ?>						
+								<?php	while($row_sector = sqlsrv_fetch_array($res_sector)){ ?>
 									<option value="<?php echo $row_sector['SECTOR']; ?>" ><?php echo $row_sector['SECTOR']; ?></option>
 								<?php } ?>
-							</select>     
-						</div>		
-                        
+							</select>
+						</div>
+
                         <div  class="col-md-2 col-sm-2 col-xs-2"><br>
 							<center><label>AÑO:</label></center>
 							<select name="usuario" class="form-control" style="text-align:center;"  onchange="es_vacio()"   id="ayo"  onBlur="es_vacio()" >
@@ -98,17 +97,17 @@
 									<option value="<?php echo @$row_ayo['ayo']; ?>" ><?php echo @$row_ayo['ayo']; ?></option>
 								<?php } ?>
 							</select>
-						</div>	
-						<div  class="col-md-2 col-sm-2 col-xs-2"><br>	
 						</div>
-							
+						<div  class="col-md-2 col-sm-2 col-xs-2"><br>
+						</div>
+
 						<div  class="col-md-5 col-sm-5 col-xs-5">
 							<center><label style="color:#337ab7; margin:0px;">PERIODO</label></center>
 							<div  class="col-md-6 col-sm-6 col-xs-6">
 								<center><label>DEL:</label></center>
 								<input type="date" name="del"  value="<?php echo $del;?>" id="del"  style="text-align:center;" onchange="es_vacio3()" class="form-control" >
-							</div>	
-							<div  class="col-md-6 col-sm-6 col-xs-6">	
+							</div>
+							<div  class="col-md-6 col-sm-6 col-xs-6">
 								<center><label>AL:</label></center>
 								<input type="date" name="al"  value="<?php echo $al;?>" id="al" style="text-align:center;" onchange="es_vacio4()"  class="form-control" >
 							</div>
@@ -117,18 +116,18 @@
 							<button  type="button" onclick="detalle()" class="btn btn-primary center-block">BUSCAR</button>
                             <br><br>
 						</div>
-						<div id="tb3" style="display: none;"></div> 
+						<div id="tb3" style="display: none;"></div>
 
-                    </div>                                    
-                </div>                
+                    </div>
+                </div>
             </section>
             </div>
-            
+
             <?php include_once '../footer.html'; ?>
             <script>
 			function detalle(){
 				var url = "<?php echo BASE_URL; ?>includes/FACTURACION/sec_txt.php";
-			
+
 				$.ajax({
 					type: "POST",
 					url: url,
@@ -141,15 +140,12 @@
 					success: function (data)
 					{
 						$("#tb3").html(data); // Mostrar la respuestas del script PHP.
-						document.getElementById("tb3").style.display="block";                  
+						document.getElementById("tb3").style.display="block";
 					}
 				});
-				
+
 
 		//        $('#myModaldestto').modal('show');
 
 			}
             </script>
-
-
-
