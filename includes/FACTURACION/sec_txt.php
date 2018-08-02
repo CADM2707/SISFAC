5b95e62865843868c258c146fd76ddf7fa41db33
CADM2707 <alex.diaz.2707@gmail.com>
2018-08-02 11:10:08 -0500

    Cambios Edrei 02-08-18

--- includes/FACTURACION/sec_txt.php
+++ includes/FACTURACION/sec_txt.php
@@ -4,10 +4,15 @@ $conn = connection_object();
 $format="d/m/Y";
 
 
+<<<<<<< HEAD
+=======
+<<<<<<< HEAD
+>>>>>>> cambios_edrei
 @$ayo=$_REQUEST['Ayo'];
 @$sector=$_REQUEST['Sector'];
 @$del=$_REQUEST['Del'];
 @$al=$_REQUEST['Al'];
+<<<<<<< HEAD
 
 			
 if($ayo != ""){ @$uno = " AND AYO=$ayo "; } else { @$uno = ""; }
@@ -27,12 +32,47 @@ $html = "";
 
 if($cuantos_son === true){
 $html.= "<form name='form_timbrado' method='post'>";
+=======
+
+			
+if($ayo != ""){ @$uno = " AND AYO=$ayo "; } else { @$uno = ""; }
+if($sector != ""){ @$dos = " AND SECTOR=$sector "; } else { @$dos = ""; }
+if($del != "" and $al != ""){ @$tres = " AND (PERIODO_INICIO between '$del' and '$al' or PERIODO_FIN between '$del' and '$al') "; } else { @$tres = ""; }
+
+$sql_reporte ="SELECT AYO,ID_FACTURA,SECTOR,ID_USUARIO,R_SOCIAL,TOTAL_REDONDEADO,CVE_SITUACION,PERIODO_INICIO,PERIODO_FIN 
+				FROM Factura FA
+				WHERE CVE_SITUACION IN (4) $uno $dos $tres 
+				and ID_FACTURA not in (select ID_FACTURA FROM [Facturacion].[dbo].[BitacoraTimbrado] BT where BT.ID_FACTURA = FA.ID_FACTURA and BT.AYO = FA.AYO)
+				order by AYO, ID_FACTURA desc";
+$res_reporte = sqlsrv_query($conn,$sql_reporte);
+$cuantos_son = sqlsrv_has_rows($res_reporte);
+
+// codigo html
+$html = "";
+
+if($cuantos_son === true){
+$html.= "<form name='form_timbrado' method='post'>";
+=======
+ @$ayo=$_REQUEST['Ayo'];
+ @$sector=$_REQUEST['Sector'];
+ @$del=$_REQUEST['Del'];
+ @$al=$_REQUEST['Al'];
+ $html = "";
+ 
+ // Consulta de la tabla
+
+				//$count_reporte=sqlsrv_fetch_array($res_reporte);	
+				//if($count_reporte>0){ 
+ 
+ // codigo html
+>>>>>>> 4429abca44dcb3015a82c3f3cae8b181bc7e2ea5
+>>>>>>> cambios_edrei
 $html.=" <br>
        <table class='table table-responsive' border='1' cellpadding='0' cellspacing='1' bordercolor='#000000' style='border-collapse:collapse;border-color:#ddd;font-size:12px;'>
 					<thead>
 					  <tr style='background-color:#337ab7; color:white; '>
 						<th><center>AÑO</center></th>
-						<th><center>ID RECIBO</center></th>
+						<th><center>ID FACTURA</center></th>
 						<th><center>SECTOR</center></th>
 						<th><center>ID USUARIO</center></th>
 						<th><center>R. SOCIAL</center></th>
@@ -63,6 +103,10 @@ $html.=" <br>
 								
 							if($i%2==0){ $color='background-color:#E1EEF4';	}else{	$color='background-color:#FFFFFF';	}				
 																							
+<<<<<<< HEAD
+=======
+<<<<<<< HEAD
+>>>>>>> cambios_edrei
 					        $html.="<tr style='$color; '>
 							<td><center> $ayo </center></td>
 							<td><center> $id </center></td>
@@ -76,13 +120,52 @@ $html.=" <br>
 							$html.= "<td><center>";
 							$html.="<input type='submit' name='gTimbrado' value='GENERAR TXT' class='btn btn-primary btn-sm center-block' formaction='archivotimbrado.php?ayo=$ayo&recibo=$id'>";
 							$html.="</center></td>
+<<<<<<< HEAD
+=======
+=======
+					    $html.="<tr style='$color; '>
+							<td><center> $a </td>
+							<td><center> $id </td>
+							<td><center> $sec </td>
+							<td><center> $usu </td>
+							<td><center> ".htmlentities ($rsoc)." </td>
+							<td><center> $tot </td>
+							<td><center> $per1 </td>
+							<td><center> $per2 </td>
+							<td><center>"; if($sit==4){ $html.="
+							<center><button type='button' class='btn btn-primary btn-sm' data-toggle='modal' onclick='variable( ".$a." , ".$usu." )' data-target='#myModal'>GENERAR TXT</button></center>"; }elseif ($sit==5){ $html.="<img src='../dist/img/verde.png' height='20'>";
+							 } $html.="</center></td>
+							<td><center>"; if($sit==5){ 
+							$html.="<a href='excel_facturacion.php' class='btn btn-success btn-xs center-block'>DESCARGAR</a>
+							 "; }else{	$html."<img src='../dist/img/rojo.png' height='20'>"; } $html.="</center></td>
+>>>>>>> 4429abca44dcb3015a82c3f3cae8b181bc7e2ea5
+>>>>>>> cambios_edrei
 					  </tr>";
 					  $i++;
 					}	  	
+<<<<<<< HEAD
+=======
+<<<<<<< HEAD
+>>>>>>> cambios_edrei
 					 $html.="<tr><td colspan='8'><center><br><br>&nbsp;</td>";
 					 $html.="<td><center><br>";
 					 $html.="<input type='submit' name='gmasivo' value='GENERAR TXT MASIVO' class='btn btn-info btn-sm center-block' formaction='archivomasivo.php?Ayo=$ayo&Sector=$sector&Del=$del&Al=$al'";
 					 $html.="<br></center></td></tr>	
+<<<<<<< HEAD
+=======
+=======
+					  $html.="<tr>
+							<td colspan='8'><center> </td>
+							
+							<td><center>"; if($b>1){ 
+							$html.="<a href='excel_facturacion.php'  class='btn btn-info btn-xs center-block'>MASIVO GENERAR TXT</a>";
+							 }else{	$html.="<img src='../dist/img/rojo.png' height='20'>"; } $html.="</center></td>
+							
+							<td><center><button type='button' class='btn btn-info btn-xs' data-toggle='modal' data-target='#myModal'>MASIVO DESCARGAR</button></center> </td>
+							
+					</tr>	
+>>>>>>> 4429abca44dcb3015a82c3f3cae8b181bc7e2ea5
+>>>>>>> cambios_edrei
 					</tbody>
 				  </table>"; 
 		$html.= "</form>";
