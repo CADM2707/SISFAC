57eb35a67166e230209b0ff597ad1538c8ed8b31
CADM2707 <alex.diaz.2707@gmail.com>
2018-08-08 11:55:44 -0500

    buzon

--- includes/Buzon/sendMail.php
+++ includes/Buzon/sendMail.php
@@ -14,14 +14,7 @@ if ($asunto != "" and $contenido != "" and $destinatario != "") {
 
     if ($_SESSION['CLIENTE'] == "SI") {
         $idOp = $_SESSION['NOMBRE'];
-        $queryOp = "declare @max as int
-            select @max=isnull(MAX(id_registro)+1,1) from BUZON
-            insert into BUZON
-                    (ID_REGISTRO,REMITENTE,ASUNTO,CVE_ESTADO,SINTESIS)
-            values
-                    (@max,'$idOp','$asunto',1,'$contenido')
-                    select @max
-            ";
+        $query = "sp_Buzon_guarda '$idOp','$asunto','$contenido'";
     } else {
         $queryOp = "select ID_OPERADOR,ID_PROGRAMA from BITACORA.DBO.Programa_Perfil where (ID_PROGRAMA=41 or ID_PROGRAMA=72 ) and ID_OPERADOR='$idOp'";
         $execue = sqlsrv_query($conn, $queryOp);
@@ -32,29 +25,21 @@ if ($asunto != "" and $contenido != "" and $destinatario != "") {
                 $idOp = "COBRANZA";
             }
 
-            $query = "declare @max as int
-            select @max=isnull(MAX(id_registro)+1,1) from BUZON
-            insert into BUZON
-                    (ID_REGISTRO,REMITENTE,ASUNTO,CVE_ESTADO,SINTESIS)
-            values
-                    (@max,'$idOp','$asunto',1,'$contenido')
-                    select @max as ID_REGISTRO
-            ";
+             $query = "sp_Buzon_guarda '$idOp','$asunto','$contenido'";
         }
     }
-    
-    $execute = sqlsrv_query($conn, $query);
-    echo $query;
-    $row = sqlsrv_fetch_array($execute);
-    echo $row['ID_REGISTRO'];
-    if ( $id_registro = $row['ID_REGISTRO'] !="" ) {
-       
-        $query = "";        
 
-        for ($i = 0; $i <= count($destinatario); $i++) {
-            $query = "insert into Buzon_Destinatario values ($id_registro,'" . $destinatario[$i] . "')";            
-            echo $insertDest =sqlsrv_query($conn,$query);
-        }               
+    $execute = sqlsrv_query($conn,$query);    
+    $row = sqlsrv_fetch_array($execute);
+    echo $id_registro=$row['ID_REGISTRO'];
+    if ($id_registro != "") {
+
+        $query = "";
+
+        for ($i = 0; $i < count($destinatario); $i++) {
+            echo $query = "insert into Buzon_Destinatario values ($id_registro,'" . $destinatario[$i] . "')";
+            $insertDest = sqlsrv_query($conn, $query);
+        }
     }
 }
 
