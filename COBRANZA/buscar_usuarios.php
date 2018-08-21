3e0b6797abada34615fbbd43b2be34d9453aa845
ehernandezg1 <35604490+ehernandezg1@users.noreply.github.com>
2018-08-20 20:40:09 -0500

    EHG 2039 20082018

--- COBRANZA/buscar_usuarios.php
+++ COBRANZA/buscar_usuarios.php
@@ -10,10 +10,10 @@ $mensaje = "";
 //Comprueba si $consultaBusqueda está seteado
 if(isset($consultaBusqueda)){
 	//Selecciona todo de la tabla de BD 
-	$consulta = "SELECT ID_USUARIO,R_SOCIAL, SECTOR, DESTACAMENTO 
+	$consulta = "SELECT ID_USUARIO,R_SOCIAL, SECTOR, DESTACAMENTO, REGION, TIPO_USUARIO, TIPO_SECTOR 
 	             FROM V_usuario_padron 
 				 WHERE CVE_SITUACION = 1 AND (ID_USUARIO LIKE '$consultaBusqueda%' OR R_SOCIAL LIKE '%$consultaBusqueda%')
-				 GROUP BY ID_USUARIO,R_SOCIAL, SECTOR, DESTACAMENTO ORDER BY ID_USUARIO";
+				 GROUP BY ID_USUARIO,R_SOCIAL, SECTOR, DESTACAMENTO, REGION, TIPO_USUARIO, TIPO_SECTOR ORDER BY ID_USUARIO";
     $res_usuario1 = sqlsrv_query($conn,$consulta);
 	$filas = sqlsrv_has_rows($res_usuario1);
 	
@@ -27,11 +27,13 @@ if(isset($consultaBusqueda)){
 		$mensaje .= "<table class='table table-responsive' border='1' cellpadding='0' cellspacing='1' bordercolor='#000000' style='border-collapse:collapse;border-color:#ddd;font-size:12px;'>";
 		$mensaje .= "<thead>";
 		$mensaje .= "<tr>";
-		$mensaje .= "<td align='center' class='bg-gray'><b>ID USUARIO</b></td>";
-		$mensaje .= "<td align='center' class='bg-gray'><b>RAZON SOCIAL</b></td>";
-		$mensaje .= "<td align='center' class='bg-gray'><b>SECTOR</b></td>";
-		$mensaje .= "<td align='center' class='bg-gray'><b>DESTACAMENTO</b></td>";
-		$mensaje .= "<td align='center' class='bg-gray'><b></b></td>";
+		$mensaje .= "<td align='center'><b>ID USUARIO</b></td>";
+		$mensaje .= "<td align='center'><b>RAZON SOCIAL</b></td>";
+		$mensaje .= "<td align='center'><b>SECTOR</b></td>";
+		$mensaje .= "<td align='center'><b>DESTACAMENTO</b></td>";
+		$mensaje .= "<td align='center'><b>REGIÓN</b></td>";
+		$mensaje .= "<td align='center'><b>TIPO USUARIO</b></td>";
+		$mensaje .= "<td align='center'><b>TIPO SECTOR</b></td>";
 		$mensaje .= "</tr>";
 		$mensaje .= "</thead>";
 		$mensaje .= "<tbody>";
@@ -45,6 +47,9 @@ if(isset($consultaBusqueda)){
 			$rsocial = $resultados['R_SOCIAL'];
 			$sector = $resultados['SECTOR'];
 			$destacamento = $resultados['DESTACAMENTO'];
+			$region = $resultados['REGION'];
+			$tusuario = $resultados['TIPO_USUARIO'];
+			$tsector = $resultados['TIPO_SECTOR'];
 
 			//Output
 			$mensaje .= "<tr bgcolor='".$color."'>";
@@ -52,7 +57,9 @@ if(isset($consultaBusqueda)){
 			$mensaje .= "<td>".utf8_encode($rsocial)."</td>";
 			$mensaje .= "<td>".$sector."</td>";
 			$mensaje .= "<td>".$destacamento."</td>";
-			$mensaje .= "<td>"."<input name='subep***".$resultados['ID_USUARIO']."' id='subep' type='submit' value='ASIGNAR PAGO' class='btn btn-primary btn-sm center-block' />"."</td>";
+			$mensaje .= "<td>".utf8_encode($region)."</td>";
+			$mensaje .= "<td>".utf8_encode($tusuario)."</td>";
+			$mensaje .= "<td>".utf8_encode($tsector)."</td>";
 			$mensaje .= "</tr>";
 						
 		};//Fin while $resultados
