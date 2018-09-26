6fa928d2c2f61241145636ccb1eefc9513ec2287
nayelidejesus <35151360+nayelidejesus@users.noreply.github.com>
2018-09-26 09:31:24 -0500

    cambios tablero de control

--- includes/FACTURACION/sec_control.php
+++ includes/FACTURACION/sec_control.php
@@ -6,15 +6,22 @@ $idOp = $_SESSION['ID_OPERADOR'];
 
  @$id=$_REQUEST['usuario'];
  @$usu=$_REQUEST['usu'];
- @$fac=$_REQUEST['fac'];
+ if(@$_REQUEST['fac'] != ""){ @$fac=$_REQUEST['fac']; }else {@$fac=NULL;}
+ if(@$_REQUEST['format'] != ""){ @$format=$_REQUEST['format']; }else {@$format='NULL';}
+ if(@$_REQUEST['per'] != ""){ @$per=$_REQUEST['per']; }else {@$per='NULL';}
+ if(@$_REQUEST['tur'] != ""){ @$tur=$_REQUEST['tur']; }else {@$tur='NULL';}
+ if(@$_REQUEST['jerar'] != ""){ @$jerar=$_REQUEST['jerar']; }else {@$jerar='NULL';}
+ if(@$_REQUEST['adi'] != ""){ @$adi=$_REQUEST['adi']; }else {@$adi='NULL';}
+ if(@$_REQUEST['banco'] != ""){ @$banco=$_REQUEST['banco']; }else {@$banco='NULL';}
+ /*@$fac=$_REQUEST['fac'];
  @$format=$_REQUEST['format'];
  @$per=$_REQUEST['per'];
  @$tur=$_REQUEST['tur'];
  @$jerar=$_REQUEST['jerar'];
- @$adi=$_REQUEST['adi'];
+ @$adi=$_REQUEST['adi'];*/
  @$correo=$_REQUEST['correo'];
  @$cuenta=$_REQUEST['cuenta'];
- @$banco=$_REQUEST['banco'];
+ //@$banco=$_REQUEST['banco'];
 @$html= "";
  $sql_reporte ="execute Facturacion.dbo.sp_guarda_Parametros '$id','$usu',$fac,$format,$per,$tur,$jerar,$adi,'$correo','$cuenta',$banco,$idOp";
  $res_reporte = sqlsrv_query($conn,$sql_reporte);
